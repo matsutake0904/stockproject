57f6b63d9ef1005565937320af7a48ca2b258a2b
matsutake0904 <62604920+matsutake0904@users.noreply.github.com>
2021-01-30 10:14:58 +0900

    Update settings.py

--- stockproject/settings.py
+++ stockproject/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'stockmodel',
         'USER': 'ryo',
-        'PASSWORD': 'maryo1994',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
     }
